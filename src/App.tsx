e68e10495ab218b805c82ec9b35fe87a02d22035
Vidhi Thakur <72219676+vidhi-thakur@users.noreply.github.com>
2025-03-07 19:52:10 +0530

    ui/added horizontal story list

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
 import React from 'react';
 import './App.css';
+import Story from './ui/Story';
 
 function App() {
   return (
     <div className="App">
       <div className='mobile'>
-        <section className='stories'></section>
+        <section className='stories'>
+          <div>
+            <Story />
+            <Story />
+            <Story />
+            <Story />
+            <Story />
+            <Story />
+            <Story />
+          </div>
+        </section>
         <section className='posts'></section>
 
       </div>
