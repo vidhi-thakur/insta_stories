59241cce10060bc646030575347406d60af57ad8
Vidhi Thakur <72219676+vidhi-thakur@users.noreply.github.com>
2025-03-09 16:41:33 +0530

    removed progressbar

--- src/ui/StoryViewer.tsx
+++ src/ui/StoryViewer.tsx
@@ -1,4 +1,4 @@
-import { CircularProgress, LinearProgress } from '@mui/material';
+import { CircularProgress } from '@mui/material';
 import CloseRoundedIcon from '@mui/icons-material/CloseRounded';
 import ArrowForwardIosRoundedIcon from '@mui/icons-material/ArrowForwardIosRounded';
 import './StoryViewer.css'
@@ -61,7 +61,7 @@ export default function StoryViewer({
     const handlePrev = () => {
         if (currImageIndex > 0) {
             setCurrImageIndex(i => i - 1)
-        } else if(currentUserStory.hasPrev) {
+        } else if (currentUserStory.hasPrev) {
             // make API all for prev story
             handlePrevStory(currentUserStory.id)
         }
@@ -77,11 +77,6 @@ export default function StoryViewer({
     return (
         <div className="storyViewer">
             <div>
-                <section className='progressbar_outer'>
-                    {currentUserStory.images.map(val => <div key={val} className='progressbar'>
-                        <LinearProgress variant="determinate" color="inherit" value={0} />
-                    </div>)}
-                </section>
                 <section className='header'>
                     <div className='userImage'>
                         <img src='/images/female-avatar.png' alt='user avatar' />
@@ -93,8 +88,8 @@ export default function StoryViewer({
             <section className='content'>
                 <img src={currentUserStory.images[currImageIndex]} alt='single story' />
             </section>
-            <ArrowForwardIosRoundedIcon onClick={handleNext} color={currentUserStory.hasMore ? 'inherit' : 'disabled'} className='icon nextIcon' />
-            <ArrowBackIosNewRoundedIcon onClick={handlePrev} color={currentUserStory.hasPrev ? 'inherit' : 'disabled'} className='icon prevIcon' />
+            {currentUserStory.hasMore ? <ArrowForwardIosRoundedIcon onClick={handleNext} className='icon nextIcon' /> : null}
+            {currentUserStory.hasPrev ? <ArrowBackIosNewRoundedIcon onClick={handlePrev} className='icon prevIcon' /> : null}
         </div>
     )
 }
